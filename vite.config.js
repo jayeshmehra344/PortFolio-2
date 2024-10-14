d0682ef68cd0e397cdba0d85e689dafea066adc9
jayeshmehra344 <jayeshmehra344@gmail.com>
2024-10-14 17:24:41 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/PortFolio-2/', // Replace 'PortFolio-2' with your actual repository name
+});
